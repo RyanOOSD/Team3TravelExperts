2962f399914f9ad89e9aba0330ade0d90c7833dc
WARHE8D-2 <59991459+WARHE8D@users.noreply.github.com>
2024-10-22 16:47:09 -0600

    booking storage and registration validation complete

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.listen(port, () => {
 
 db.connect((err) => {
   if (err) throw err;
-    console.log("db connected");
+  console.log("db connected");
 });
 
 //for / and home to used as same
@@ -52,25 +52,33 @@ app.get(["/", "/home"], (req, res) => {
 
   db.query(query, (err, result) => {
     if (err) throw err;
-      console.log(result);
-      res.render("index", {pageTitle: "Home | Travel Experts", package: result });
+    console.log(result);
+    res.render("index", {
+      pageTitle: "Home | Travel Experts",
+      package: result,
+    });
   });
 });
 
 // gets the agent contact details to contact page
 app.get("/contact", (req, res) => {
-  const agencyQuery = "SELECT * FROM agencies"
+  const agencyQuery = "SELECT * FROM agencies";
   const agentQuery = "SELECT * from agents WHERE AgencyId = 1";
   const agentTwoQuery = "SELECT * from agents WHERE AgencyId = 2";
 
   db.query(agencyQuery, (err, agencyOut) => {
     if (err) throw err;
-      db.query(agentQuery, (err, agentOut) => {
-        if (err) throw err;
-          db.query(agentTwoQuery, (err, agentTwoOut) => {
-            res.render("contact", {pageTitle: "Contact Us | Travel Experts", agencies: agencyOut, agents: agentOut, agentTwo: agentTwoOut});
-          });
+    db.query(agentQuery, (err, agentOut) => {
+      if (err) throw err;
+      db.query(agentTwoQuery, (err, agentTwoOut) => {
+        res.render("contact", {
+          pageTitle: "Contact Us | Travel Experts",
+          agencies: agencyOut,
+          agents: agentOut,
+          agentTwo: agentTwoOut,
+        });
       });
+    });
   });
 });
 
@@ -80,48 +88,51 @@ app.get("/booking", (req, res) => {
   const query = "SELECT * FROM packages WHERE PackageId = ?";
   const agentQuery = "SELECT * FROM agents";
 
-  db.query(query, [packageId],  (err, results) => {
+  db.query(query, [packageId], (err, results) => {
     if (err) throw err;
-      db.query(agentQuery, (err, agentOut) => {
-       res.render("booking", {pageTitle: "Booking Form | Travel Experts", package: results[0], agents: agentOut });
+    db.query(agentQuery, (err, agentOut) => {
+      res.render("booking", {
+        pageTitle: "Booking Form | Travel Experts",
+        package: results[0],
+        agents: agentOut,
+      });
     });
   });
 });
 
-app.post(
-  "/submit-booking",
-  [
-    [
-      body("travelerCount", "Invalid traveler count").isNumeric().isLength({max: 1}),
-      body("packageId", "Invalid package ID").isNumeric().isLength({max: 1}),
-      body("firstName", "Invalid first name").isAlpha("en-US", {ignore: "\s.-'"}).isLength({max: 25}),
-      body("lastName", "Invalid last name").isAlpha("en-US", {ignore: "\s.-'"}).isLength({max: 25}),
-      body("address", "Invalid address").isAlphanumeric("en-US", {ignore: "\s.-'"}).isLength({max: 75}),
-      body("city", "Invalid city").isAlpha("en-US", {ignore: "\s.-'"}).isLength({max: 50}),
-      body("province", "Invalid province").isAlpha().isLength({max: 2}),
-      body("postalCode", "Invalid postal code").isAlphanumeric("en-US", {ignore: "\s"}).isLength({max: 7}),
-      body("country", "Invalid country").isAlpha("en-US", {ignore: "\&\s-'"}).isLength({max: 25}),
-      body("homePhone", "Invalid home phone").isNumeric().isLength({max: 20}),
-      body("busPhone", "Invalid business phone").isNumeric().isLength({max: 20}),
-      body("email", "Invalid email").isEmail().isLength({max: 50}),
-      body('agentId', "Invalid agent").isNumeric().isLength({max: 1}),
-    ],
-  ],
-  async (req, res) => {
-    console.log("submit-booking", req.body);
-    const error = validationResult(req);
-    const pullErr = validationResult.withDefaults({
-        formatter: error => error.msg,
-    });
-    const printErr = pullErr(req).array();
+app.post("/submit-booking", async (req, res) => {
+  console.log("submit-booking", req.body);
+  const {
+    travelerCount,
+    packageId,
+    firstName,
+    lastName,
+    address,
+    city,
+    province,
+    postalCode,
+    country,
+    homePhone,
+    busPhone,
+    email,
+    agentId,
+    tripType,
+  } = req.body;
 
-    if (!error.isEmpty()) {
-      return res.render("badform", {pageTitle: "Invalid form!", badSubmit: printErr});
-    }
+  try {
+    // random customer and booking id generation
+    const customerId = generateNo();
+    const bookingId = generateNo();
+    const bookingNo = generateNo();
+    // const tripType = "L"; // Fixed value for leisure trip
 
-    const {
-      travelerCount,
-      packageId,
+    // Insert customer into `customers` table
+    const customerQuery = `
+        INSERT INTO customers (CustomerId,CustFirstName, CustLastName, CustAddress, CustCity, CustProv, CustPostal, CustCountry, CustHomePhone, CustBusPhone, CustEmail, AgentId)
+        VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+      `;
+    const customerValues = [
+      customerId,
       firstName,
       lastName,
       address,
@@ -133,99 +144,55 @@ app.post(
       busPhone,
       email,
       agentId,
-    } = req.body;
+    ];
+    //You have tried to call .then(), .catch(), or invoked await on the result of query that is not a promise, which is a programming error. Try calling con.promise().query(),
+    await db.promise().query(customerQuery, customerValues);
 
-    try {
-      const bookingNo = generateNo();
-      const tripType = "L"; // Fixed value for leisure trip
-
-      // Insert customer into `customers` table
-      const customerQuery = `
-        INSERT INTO customers (CustFirstName, CustLastName, CustAddress, CustCity, CustProv, CustPostal, CustCountry, CustHomePhone, CustBusPhone, CustEmail, AgentId)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    // Get the customer that was just created and input the ID into the booking table
+    const newCustomer =
+      "SELECT CustomerId FROM customers ORDER BY CustomerID DESC LIMIT 1";
+    const newId = await db.promise().query(newCustomer);
+    // const customerId = JSON.stringify(newId[0][0].CustomerId);
+    // Insert booking into `bookings` table
+    const bookingQuery = `
+        INSERT INTO bookings (BookingId, BookingDate, BookingNo, TravelerCount, CustomerId, TripTypeId, PackageId)
+        VALUES (?,NOW(), ?, ?, ?, ?, ?)
       `;
-      const customerValues = [
-        firstName,
-        lastName,
-        address,
-        city,
-        province,
-        postalCode,
-        country,
-        homePhone,
-        busPhone,
-        email,
-        agentId,
-      ];
-      //You have tried to call .then(), .catch(), or invoked await on the result of query that is not a promise, which is a programming error. Try calling con.promise().query(),
-      await db.promise().query(customerQuery, customerValues);
+    const bookingValues = [
+      bookingId,
+      bookingNo,
+      travelerCount,
+      customerId,
+      tripType,
+      packageId,
+    ];
+    await db.promise().query(bookingQuery, bookingValues);
 
-      // Get the customer that was just created and input the ID into the booking table
-      const newCustomer = "SELECT CustomerId FROM customers ORDER BY CustomerID DESC LIMIT 1";
-      const newId = await db.promise().query(newCustomer);
-      const customerId = JSON.stringify(newId[0][0].CustomerId);
-      // Insert booking into `bookings` table
-      const bookingQuery = `
-        INSERT INTO bookings (BookingDate, BookingNo, TravelerCount, CustomerId, TripTypeId, PackageId)
-        VALUES (NOW(), ?, ?, ?, ?, ?)
-      `;
-      const bookingValues = [
-        bookingNo,
-        travelerCount,
-        customerId,
-        tripType,
-        packageId,
-      ];
-      await db.promise().query(bookingQuery, bookingValues);
-
-      console.log("Booking and customer data saved successfully");
-      res.render("thankbook");
-    } catch (error) {
-      console.error("Error saving data:", error);
-      res.status(500).send("Error processing booking.");
-    }
+    console.log("Booking and customer data saved successfully");
+    res.render("thankbook");
+  } catch (error) {
+    console.error("Error saving data:", error);
+    res.status(500).send("Error processing booking.");
   }
-);
+});
 
 // Endpoint to serve registration page
-app.get("/register", async(req, res) => {
+app.get("/register", async (req, res) => {
   const agentQuery = "SELECT * FROM agents";
-  db.query(agentQuery, (err, agentOut) => { 
-    res.render("register", {pageTitle: "Register | Travel Experts", agents: agentOut});
+  db.query(agentQuery, (err, agentOut) => {
+    res.render("register", {
+      pageTitle: "Register | Travel Experts",
+      agents: agentOut,
+    });
   });
 });
 
 // Endpoint to handle registration submissions
 app.post(
   "/submit-registration",
-  [
-    [
-      body("firstName", "Invalid first name").isAlpha("en-US", {ignore: "\s.-'"}).isLength({max: 25}),
-      body("lastName", "Invalid last name").isAlpha("en-US", {ignore: "\s.-'"}).isLength({max: 25}),
-      body("address", "Invalid address").isAlphanumeric("en-US", {ignore: "\s.-'"}).isLength({max: 75}),
-      body("city", "Invalid city").isAlpha("en-US", {ignore: "\s.-'"}).isLength({max: 50}),
-      body("province", "Invalid province").isAlpha().isLength({max: 2}),
-      body("postalCode", "Invalid postal code").isAlphanumeric("en-US", {ignore: "\s"}).isLength({max: 7}),
-      body("country", "Invalid country").isAlpha("en-US", {ignore: "\&\s-'"}).isLength({max: 25}),
-      body("homePhone", "Invalid home phone").isNumeric().isLength({max: 20}),
-      body("busPhone", "Invalid business phone").isNumeric().isLength({max: 20}),
-      body("email", "Invalid email").isEmail().isLength({max: 50}),
-      body("username", "Invalid username").isAlphanumeric().isLength({min: 3, max: 20}),
-      body("password", "Invalid password").isAlphanumeric("en-US", {ignore: "!@#$%^&*"}).isLength({min: 8, max: 24}),
-      body('agentId', "Invalid agent").isNumeric().isLength({max: 1}),
-    ],
-  ],
-  async(req, res) => {
-    const error = validationResult(req);
-    const pullErr = validationResult.withDefaults({
-        formatter: error => error.msg,
-    });
-    const printErr = pullErr(req).array();
 
-    if (!error.isEmpty()) {
-      return res.render("badform", {pageTitle: "Invalid form!", badSubmit: printErr});
-    }
-    const { 
+  async (req, res) => {
+    const {
       firstName,
       lastName,
       email,
@@ -239,7 +206,7 @@ app.post(
       username,
       password,
       agentId,
-    } = req.body
+    } = req.body;
     const registerCustomer = `
       INSERT INTO customers (CustFirstName, CustLastName, CustAddress, CustCity, CustProv, CustPostal, CustCountry, CustHomePhone, CustBusPhone, CustEmail, CustUser, CustPass, AgentId)
       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
@@ -259,15 +226,18 @@ app.post(
       password,
       agentId,
     ];
-    await db.promise().query(registerCustomer, customerInput)
-    console.log(agentId)
+    await db.promise().query(registerCustomer, customerInput);
+    console.log(agentId);
     res.render("thankregister");
   }
 );
 
 // Create generic endpoint to serve error for invalid requests
 app.use((req, res) => {
-  res.render("error", {pageTitle: "Error! | Travel Experts", invalidUrl: req.url})
+  res.render("error", {
+    pageTitle: "Error! | Travel Experts",
+    invalidUrl: req.url,
+  });
 });
 
 // Function to generate a random Number
